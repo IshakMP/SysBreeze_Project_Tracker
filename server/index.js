0b1e1f2479dddcd2e89eccd92554c6e6ba8ee06a
IshakMP <ishqmp@gmail.com>
2023-09-14 15:30:14 +0530

    GraphQl Minimum implemented

---
+++ server/index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const schema = require("./schema/schema");
+const connectDB = require("./config/db");
+
+const app = express();
+connectDB();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+const port = process.env.PORT || 3000;
+app.listen(port, console.log(`Server running on port ${port}`));
